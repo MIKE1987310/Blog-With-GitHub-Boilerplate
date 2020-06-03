41369240714b5717ab782b0004718857ac05ec4b
MIKE1987310 <47750054+MIKE1987310@users.noreply.github.com>
2020-06-03 16:49:44 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -14,8 +14,8 @@ template = {
     "path": "../Galileo"
 }
 enable_jsdelivr = {
-    "enabled": False,
-    "repo": ""
+    "enabled": True,
+    "repo": "MIKE1987310/Blog-With-GitHub-Boilerplate@gh-pages"
 }
 
 # 站点设置
